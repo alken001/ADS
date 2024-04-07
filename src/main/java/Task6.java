911daa5016e985ed1a51c7cc2f20ba415a80cf6d
zaira <231128@astanait.edu.kz>
2024-04-07 22:58:45 +0500

    Task6

--- src/main/java/Task6.java
+++ src/main/java/Task6.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Task6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(findAPowN(a,n));
+    }
+    //Complexity: linear - 0(N)
+    public static int findAPowN(int a, int n){
+        int result = 1;
+        for(int i = 0; i<n; i++) {
+            result *= a;
+        }
+        return result;
+    }
 }
