11e8c2e1eef6c32896e7785f61e411bc9085da53
zaira <231128@astanait.edu.kz>
2024-04-07 23:00:29 +0500

    Task10

--- src/main/java/Task10.java
+++ src/main/java/Task10.java
@@ -1,2 +1,21 @@
+
+import java.util.Scanner;
+
 public class Task10 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(gcd(a, b));
+    }
+    //Complexity: logarithmic - 0(log(N))
+    public static int gcd(int a, int b) {
+
+        if (b == 0)
+            return a;
+
+        return gcd(b, a % b);
+    }
 }
