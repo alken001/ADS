8cbc0f1f374130b1a876b12c7379fae2deb70827
zaira <231128@astanait.edu.kz>
2024-04-07 22:56:47 +0500

    Task3

--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(findPrimeOrComposite(n));
+    }
+    //Complexity: linear - 0(N)
+    public static String findPrimeOrComposite(int n) {
+        if (n == 1) {
+            return "Neither prime nor composite";
+        }
+        for (int i = 2; i < n; i++) {
+            if(n % i == 0){
+                return "Composite";
+            }
+            else{
+                return "Prime";
+            }
+        }
+        return null;
+    }
 }
