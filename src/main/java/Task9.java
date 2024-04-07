239bcd5086635900a9f5bc328ec7375fe968279a
zaira <231128@astanait.edu.kz>
2024-04-07 22:59:54 +0500

    Task9

--- src/main/java/Task9.java
+++ src/main/java/Task9.java
@@ -1,2 +1,19 @@
+
+import java.util.Scanner;
+
 public class Task9 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        System.out.println(binomialCoefficient(n, k));
+    }
+    //Complexity: exponential - 0(2^N)
+    public static int binomialCoefficient(int n, int k) {
+        if (k == 0 || k == n)
+            return 1;
+
+        return binomialCoefficient(n - 1, k -1) + binomialCoefficient(n -1, k);
+    }
 }
