f1897dd58de455f8e1441f45b8d54170d3ba8ce6
zaira <231128@astanait.edu.kz>
2024-04-07 22:57:36 +0500

    Task5

--- src/main/java/Task5.java
+++ src/main/java/Task5.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(findFibonacciElement(n));
+    }
+    //Complexity: exponential - 0(2^N)
+    public static int findFibonacciElement(int n){
+        if (n <= 1) {
+            return n;
+        }
+        return findFibonacciElement(n - 1) + findFibonacciElement(n - 2);
+    }
 }
