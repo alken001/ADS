b4c4b782ff41734327303a474f2f10a67a48c9cd
zaira <231128@astanait.edu.kz>
2024-04-07 22:55:33 +0500

    Task2

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double[] arr = new double[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(findAverage(arr));
+    }
+    //Complexity: linear - 0(N)
+    public static double findAverage(double[] arr) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum / arr.length;
+    }
 }
