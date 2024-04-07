5a4c062ac398ad124ead9645d3f5f4e0e062b26b
zaira <231128@astanait.edu.kz>
2024-04-07 22:59:08 +0500

    Task7

--- src/main/java/Task7.java
+++ src/main/java/Task7.java
@@ -1,2 +1,38 @@
-public class Task7 {
+import java.util.Scanner;
+
+public class Task7{
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        permutation(str);
+    }
+    //Complexity: constant - 0(1)
+    public static String swap(String str, int i, int j) {
+
+        StringBuilder stringBuilder = new StringBuilder(str);
+        stringBuilder.setCharAt(i, str.charAt(j));
+        stringBuilder.setCharAt(j, str.charAt(i));
+        return stringBuilder.toString();
+    }
+    //Complexity: linear - 0(N)
+    private static void permutation(String str, int i) {
+
+        if (i == str.length()) {
+            System.out.println(str);
+            return;
+        }
+
+        for (int j = i; j < str.length(); j++) {
+            str = swap(str, i, j);
+            permutation(str, i + 1);
+            str = swap(str, i, j);
+        }
+    }
+
+    public static void permutation(String str) {
+        permutation(str, 0);
+    }
+
 }
