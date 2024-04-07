d228640290cd15497110a33c94615152fec7fe46
zaira <231128@astanait.edu.kz>
2024-04-07 22:57:07 +0500

    Task3

--- src/main/java/Task4.java
+++ src/main/java/Task4.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Task4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(findFactorial(n));
+    }
+    //Complexity: linear - 0(N)
+    public static int findFactorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return findFactorial(n-1)*n;
+    }
 }
